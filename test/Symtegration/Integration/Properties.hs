f971b9323bccf4cb441009dea566959d06fba43e
Yoo Chung <dev@chungyc.org>
2024-12-13 19:21:54 +0000

    Make antiderivative property general.  Allow use of custom generators.

---
+++ test/Symtegration/Integration/Properties.hs
@@ -0,0 +1,50 @@
+-- |
+-- Description: Provides general properties that can be used to testing various integration algorithms.
+-- Maintainer: dev@chungyc.org
+module Symtegration.Integration.Properties (antiderivativeProperty) where
+
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Numeric.AD
+import Symtegration.FiniteDouble
+import Symtegration.Symbolic
+import Symtegration.Symbolic.Haskell
+import Test.Hspec
+import Test.QuickCheck
+
+-- | Tests the property that a function should be consistent
+-- with the derivative of its integral.
+antiderivativeProperty ::
+  (Text -> Expression -> Maybe Expression) ->
+  Map Text Double ->
+  Expression ->
+  Double ->
+  Property
+antiderivativeProperty integrate m e x =
+  not (Map.null m) ==> forAll (elements $ Map.keys m) $ \v -> check (integrate v e) v
+  where
+    check Nothing _ = label "integration fail" True
+    check (Just integrated) v =
+      isFinite (FiniteDouble $ f x) && isFinite (FiniteDouble $ f' x) ==>
+        label "integration success" $
+          counterexample ("derivative = " <> Text.unpack (toHaskell e)) $
+            counterexample ("antiderivative = " <> Text.unpack (toHaskell integrated)) $
+              Near (FiniteDouble (f' x)) `shouldBe` Near (FiniteDouble (f x))
+      where
+        -- The original function and the derivative of the integral should behave similarly.
+        --
+        -- These are (Double -> Double).  It seems Numeric.AD does not like FiniteDouble.
+        f = toFunction e (replace v)
+        f' = diff (toFunction integrated (replaceForDiff v))
+
+    -- Map all but the variable symbol to concrete numbers.
+    replace var s
+      | s == var = id
+      | (Just z) <- Map.lookup s m = const z
+      | otherwise = error "unknown symbol"
+    replaceForDiff var s
+      | s == var = id
+      | (Just z) <- Map.lookup s m = const $ auto z
+      | otherwise = error "unknown symbol"
--- test/Symtegration/IntegrationSpec.hs
+++ test/Symtegration/IntegrationSpec.hs
@@ -5,14 +5,12 @@ module Symtegration.IntegrationSpec (spec) where
 
 import Data.Map qualified as Map
 import Data.Text (Text)
-import Data.Text qualified as Text
-import Numeric.AD
 import Symtegration.FiniteDouble
 import Symtegration.Integration.Polynomial qualified as Polynomial
+import Symtegration.Integration.Properties qualified as Properties
 import Symtegration.Integration.Trigonometric qualified as Trigonometric
 import Symtegration.Symbolic
 import Symtegration.Symbolic.Arbitrary
-import Symtegration.Symbolic.Haskell
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
@@ -34,29 +32,5 @@ antiderivativeProperty ::
   Complete ->
   Double ->
   Property
-antiderivativeProperty integrate (Complete e m) x =
-  not (Map.null m) ==> forAll (elements $ Map.keys m) $ \v -> check (integrate v e) v
-  where
-    check Nothing _ = label "integration fail" True
-    check (Just integrated) v =
-      isFinite (FiniteDouble $ f x) && isFinite (FiniteDouble $ f' x) ==>
-        label "integration success" $
-          counterexample ("derivative = " <> Text.unpack (toHaskell e)) $
-            counterexample ("antiderivative = " <> Text.unpack (toHaskell integrated)) $
-              Near (FiniteDouble (f' x)) `shouldBe` Near (FiniteDouble (f x))
-      where
-        -- The original function and the derivative of the integral should behave similarly.
-        --
-        -- These are (Double -> Double).  It seems Numeric.AD does not like FiniteDouble.
-        f = toFunction e (replace v)
-        f' = diff (toFunction integrated (replaceForDiff v))
-
-    -- Map all but the variable symbol to concrete numbers.
-    replace var s
-      | s == var = id
-      | (Just (FiniteDouble z)) <- Map.lookup s m = const z
-      | otherwise = error "unknown symbol"
-    replaceForDiff var s
-      | s == var = id
-      | (Just (FiniteDouble z)) <- Map.lookup s m = const $ auto z
-      | otherwise = error "unknown symbol"
+antiderivativeProperty f (Complete e m) =
+  Properties.antiderivativeProperty f (Map.map (\(FiniteDouble z) -> z) m) e
