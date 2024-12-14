04ac9e4d307098174194d2b909ff3cbd700f968a
Yoo Chung <dev@chungyc.org>
2024-12-13 22:01:09 -0500

    Hook up term by term integration.

--- src/Symtegration/Integration.hs
+++ src/Symtegration/Integration.hs
@@ -3,6 +3,7 @@ module Symtegration.Integration (integrate) where
 import Data.Foldable (asum)
 import Data.Text (Text)
 import Symtegration.Integration.Polynomial qualified as Polynomial
+import Symtegration.Integration.Term qualified as Term
 import Symtegration.Integration.Trigonometric qualified as Trigonometric
 import Symtegration.Symbolic
 
@@ -11,7 +12,10 @@ import Symtegration.Symbolic
 -- its symbolic representation.  It will return 'Nothing' if it is
 -- unable to derive an integral.
 integrate :: Text -> Expression -> Maybe Expression
-integrate var expr = asum [f var expr | f <- directIntegrations]
+integrate var expr =
+  asum $
+    [f var expr | f <- directIntegrations]
+      <> [Term.integrate directIntegrations var expr]
 
 -- | List of direct integration functions.
 -- These integrate expressions directly without using other integration functions.
