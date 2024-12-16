486809dcd2fe36348f6c31d40f17f1472c0b8af7
Yoo Chung <dev@chungyc.org>
2024-12-16 21:43:35 +0000

    Add module documentation to a test.

--- test/Symtegration/Symbolic/Simplify/NumericFoldingSpec.hs
+++ test/Symtegration/Symbolic/Simplify/NumericFoldingSpec.hs
@@ -1,3 +1,8 @@
+-- |
+-- Description: Tests Symtegration.Symbolic.Simplify.NumericFolding
+-- Copyright: Copyright 2024 Yoo Chung
+-- License: Apache-2.0
+-- Maintainer: dev@chungyc.org
 module Symtegration.Symbolic.Simplify.NumericFoldingSpec (spec) where
 
 import Symtegration.Symbolic.Simplify.NumericFolding
