0302b000adfcca1ae11bc938c7ab8f170766aec0
Yoo Chung <dev@chungyc.org>
2024-12-24 18:45:55 -0500

    Use function to add parentheses.

--- src/Symtegration/Symbolic/LaTeX.hs
+++ src/Symtegration/Symbolic/LaTeX.hs
@@ -30,30 +30,33 @@ toLaTeX (BinaryApply func x y) = binary func x y
 
 -- | Converts unary functions into LaTeX.
 unary :: UnaryFunction -> Expression -> Text
-unary Negate x = "-\\left(" <> toLaTeX x <> "\\right)"
+unary Negate x = "-" <> par (toLaTeX x)
 unary Abs x = "\\left\\lvert " <> toLaTeX x <> " \\right\\rvert"
-unary Signum x = "\\mathrm{signum}\\left(" <> toLaTeX x <> "\\right)"
+unary Signum x = "\\mathrm{signum}" <> par (toLaTeX x)
 unary Exp x = "e^{" <> toLaTeX x <> "}"
-unary Log x = "\\log\\left(" <> toLaTeX x <> "\\right)"
+unary Log x = "\\log" <> par (toLaTeX x)
 unary Sqrt x = "\\sqrt{" <> toLaTeX x <> "}"
-unary Sin x = "\\sin\\left(" <> toLaTeX x <> "\\right)"
-unary Cos x = "\\cos\\left(" <> toLaTeX x <> "\\right)"
-unary Tan x = "\\tan\\left(" <> toLaTeX x <> "\\right)"
-unary Asin x = "\\arcsin\\left(" <> toLaTeX x <> "\\right)"
-unary Acos x = "\\arccos\\left(" <> toLaTeX x <> "\\right)"
-unary Atan x = "\\arctan\\left(" <> toLaTeX x <> "\\right)"
-unary Sinh x = "\\sinh\\left(" <> toLaTeX x <> "\\right)"
-unary Cosh x = "\\cosh\\left(" <> toLaTeX x <> "\\right)"
-unary Tanh x = "\\tanh\\left(" <> toLaTeX x <> "\\right)"
-unary Asinh x = "\\sinh^{-1}\\left(" <> toLaTeX x <> "\\right)"
-unary Acosh x = "\\cosh^{-1}\\left(" <> toLaTeX x <> "\\right)"
-unary Atanh x = "\\tanh^{-1}\\left(" <> toLaTeX x <> "\\right)"
+unary Sin x = "\\sin" <> par (toLaTeX x)
+unary Cos x = "\\cos" <> par (toLaTeX x)
+unary Tan x = "\\tan" <> par (toLaTeX x)
+unary Asin x = "\\arcsin" <> par (toLaTeX x)
+unary Acos x = "\\arccos" <> par (toLaTeX x)
+unary Atan x = "\\arctan" <> par (toLaTeX x)
+unary Sinh x = "\\sinh" <> par (toLaTeX x)
+unary Cosh x = "\\cosh" <> par (toLaTeX x)
+unary Tanh x = "\\tanh" <> par (toLaTeX x)
+unary Asinh x = "\\sinh^{-1}" <> par (toLaTeX x)
+unary Acosh x = "\\cosh^{-1}" <> par (toLaTeX x)
+unary Atanh x = "\\tanh^{-1}" <> par (toLaTeX x)
 
 -- | Converts binary functions into LaTeX.
 binary :: BinaryFunction -> Expression -> Expression -> Text
-binary Add x y = "\\left(" <> toLaTeX x <> "\\right) + \\left(" <> toLaTeX y <> "\\right)"
-binary Multiply x y = "\\left(" <> toLaTeX x <> "\\right) \\times \\left(" <> toLaTeX y <> "\\right)"
-binary Subtract x y = "\\left(" <> toLaTeX x <> "\\right) - \\left(" <> toLaTeX y <> "\\right)"
+binary Add x y = par (toLaTeX x) <> " + " <> par (toLaTeX y)
+binary Multiply x y = par (toLaTeX x) <> " \\times " <> par (toLaTeX y)
+binary Subtract x y = par (toLaTeX x) <> " - " <> par (toLaTeX y)
 binary Divide x y = "\\frac{" <> toLaTeX x <> "}{" <> toLaTeX y <> "}"
-binary Power x y = "\\left(" <> toLaTeX x <> "\\right)^{" <> toLaTeX y <> "}"
-binary LogBase x y = "\\log_{" <> toLaTeX x <> "}\\left(" <> toLaTeX y <> "\\right)"
+binary Power x y = par (toLaTeX x) <> "^{" <> toLaTeX y <> "}"
+binary LogBase x y = "\\log_{" <> toLaTeX x <> "}" <> par (toLaTeX y)
+
+par :: Text -> Text
+par s = "\\left(" <> s <> "\\right)"
