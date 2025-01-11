5792b9184aa846b129d2dee10462ac74a7538042
Yoo Chung <dev@chungyc.org>
2025-01-10 22:41:06 -0500

    Specifically test for equal terms in factoring.

--- test/Symtegration/Symbolic/Simplify/FactorSpec.hs
+++ test/Symtegration/Symbolic/Simplify/FactorSpec.hs
@@ -9,6 +9,7 @@ import Symtegration.Symbolic.Simplify.Factor
 import Symtegration.Symbolic.Simplify.Properties
 import Test.Hspec
 import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 spec :: Spec
 spec = parallel $ do
@@ -16,3 +17,12 @@ spec = parallel $ do
     modifyMaxSuccess (* 100) $
       prop "maintains semantics" $
         equivalentProperty simplify
+
+    prop "factors common sub-expressions" $ \x y z ->
+      x /= y && x /= z && y /= z ==>
+        conjoin
+          [ simplify ((z * x) + (z * y)) === z * (x + y),
+            simplify ((z * x) + (y * z)) === z * (x + y),
+            simplify ((x * z) + (z * y)) === z * (x + y),
+            simplify ((x * z) + (y * z)) === z * (x + y)
+          ]
