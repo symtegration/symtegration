8d30430be828f83a32d675101e55605af44b7f95
Yoo Chung <dev@chungyc.org>
2025-01-12 10:08:17 -0500

    Add function to tidy up expressions.

---
+++ src/Symtegration/Symbolic/Simplify/Tidy.hs
@@ -0,0 +1,64 @@
+-- |
+-- Module: Symtegtarion.Symbolic.Simplify.Tidy
+-- Description: Tidy up a simplified mathematical expression.
+-- Copyright: Copyright 2025 Yoo Chung
+-- License: Apache-2.0
+-- Maintainer: dev@chungyc.org
+module Symtegration.Symbolic.Simplify.Tidy (tidy) where
+
+import Symtegration.Symbolic
+
+-- $setup
+-- >>> import Symtegration.Symbolic
+-- >>> import Symtegration.Symbolic.Haskell
+
+-- | Tidies up expressions for nicer output.
+--
+-- Assumes that other simplifications have been applied first.
+-- In fact, it may undo changes that made other simplifications easier.
+--
+-- ==== What is tidied up
+--
+-- This section shows examples of what this function tidies up.
+--
+-- >>> toHaskell $ tidy $ "x" + negate "y"
+-- "x - y"
+--
+-- >>> toHaskell $ tidy $ "x" + Number (-2) * "y"
+-- "x - 2 * y"
+--
+-- >>> toHaskell $ tidy $ Number (-1) / Number 2
+-- "negate (1 / 2)"
+--
+-- >>> toHaskell $ tidy $ Number (-1) * "x"
+-- "negate x"
+--
+-- >>> toHaskell $ tidy $ (-"x") * "y"
+-- "negate (x * y)"
+--
+-- >>> toHaskell $ tidy $ "x" * (-"y")
+-- "negate (x * y)"
+--
+-- >>> toHaskell $ tidy $ (-"x") * (-"y")
+-- "x * y"
+tidy :: Expression -> Expression
+tidy (UnaryApply func x) = unary $ UnaryApply func $ tidy x
+tidy (BinaryApply func x y) = binary $ BinaryApply func (tidy x) (tidy y)
+tidy e = e
+
+unary :: Expression -> Expression
+unary e = e
+
+binary :: Expression -> Expression
+binary (x :+: (Negate' y)) = x :-: y
+binary (Number (-1) :*: x) = Negate' x
+binary e@(Number n :*: x)
+  | n < 0 = Negate' (Number (-n) :*: x)
+  | otherwise = e
+binary e@(Number n :/: x)
+  | n < 0 = Negate' (Number (-n) :/: x)
+  | otherwise = e
+binary (Negate' x :*: Negate' y) = x :*: y
+binary (Negate' x :*: y) = Negate' $ x :*: y
+binary (x :*: Negate' y) = Negate' $ x :*: y
+binary e = e
---
+++ test/Symtegration/Symbolic/Simplify/TidySpec.hs
@@ -0,0 +1,59 @@
+-- |
+-- Description: Tests Symtegration.Symbolic.Simplify.Tidy.
+-- Copyright: Copyright 2025 Yoo Chung
+-- License: Apache-2.0
+-- Maintainer: dev@chungyc.org
+module Symtegration.Symbolic.Simplify.TidySpec (spec) where
+
+import Symtegration.Symbolic
+import Symtegration.Symbolic.Arbitrary
+import Symtegration.Symbolic.Simplify.Properties
+import Symtegration.Symbolic.Simplify.Tidy
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
+spec :: Spec
+spec = parallel $ do
+  describe "tidy" $ do
+    modifyMaxSuccess (* 100) $
+      prop "maintains semantics" $
+        equivalentProperty tidy
+
+    prop "x + negate y" $
+      forAll arbitrarySymbol $ \x ->
+        forAll arbitrarySymbol $ \y ->
+          tidy (x + negate y) `shouldBe` x - y
+
+    prop "x + (-1) * y" $
+      forAll arbitrarySymbol $ \x ->
+        forAll arbitrarySymbol $ \y ->
+          tidy (x + Number (-1) * y) `shouldBe` x - y
+
+    prop "x + (-n) * y" $ \(Positive n) ->
+      n > 1 ==>
+        forAll arbitrarySymbol $ \x ->
+          forAll arbitrarySymbol $ \y ->
+            tidy (x + Number (-n) * y) `shouldBe` x - Number n * y
+
+    prop "(-n) / m" $ \(Positive n) (Positive m) ->
+      tidy (Number (-n) / Number m) `shouldBe` negate (Number n / Number m)
+
+    prop "(-n) / x" $ \(Positive n) ->
+      forAll arbitrarySymbol $ \x ->
+        tidy (Number (-n) / x) `shouldBe` negate (Number n / x)
+
+    prop "(-x) * y" $
+      forAll arbitrarySymbol $ \x ->
+        forAll arbitrarySymbol $ \y ->
+          tidy ((-x) * y) `shouldBe` negate (x * y)
+
+    prop "x * (-y)" $
+      forAll arbitrarySymbol $ \x ->
+        forAll arbitrarySymbol $ \y ->
+          tidy (x * (-y)) `shouldBe` negate (x * y)
+
+    prop "(-x) * (-y)" $
+      forAll arbitrarySymbol $ \x ->
+        forAll arbitrarySymbol $ \y ->
+          tidy ((-x) * (-y)) `shouldBe` x * y
